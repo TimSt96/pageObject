9683859afbaec1b04d9c28cb55c0c3a77e2922c8
Solovey96 <garret-tm@yandex.ru>
2023-01-08 20:07:40 +0500

    ok

---
+++ src/main/java/Common/CommonActions.java
@@ -0,0 +1,27 @@
+package Common;
+
+import org.junit.Assert;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static Common.Config.PLATFORM_AND_BROWSER;
+import static constants.Constant.TimeoutVariable.IMPLICIT_WAIT;
+
+public class CommonActions {
+    public static WebDriver createDriver() {
+        WebDriver driver = null;
+        switch (PLATFORM_AND_BROWSER) {
+            case"win_chrome":
+                System.setProperty("webdriver.chrome.driver", "C:\\Users\\garre\\IdeaProjects\\chromedriver.exe");
+                driver = new ChromeDriver();
+                break;
+            default:
+                Assert.fail("Incorrect platform or browser name: " + PLATFORM_AND_BROWSER);
+        }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(IMPLICIT_WAIT, TimeUnit.SECONDS);
+        return driver;
+    }
+}
---
+++ src/main/java/pages/realt_home/RealtHomePage.java
@@ -0,0 +1,30 @@
+package pages.realt_home;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import pages.base.BasePage;
+
+public class RealtHomePage extends BasePage {
+    public RealtHomePage(WebDriver driver) {
+        super(driver);
+    }
+
+    private final By countRooms = By.id("rooms");
+    private final By optionsToRooms = By.xpath("//*[@id=\"rooms\"]/option[3]");
+    private final By findBtn = By.xpath("//*[@id=\"residentialInputs\"]/div[1]/form/div[2]/div[2]/a[2]");
+
+    public RealtHomePage enterCountRooms() {
+        driver.findElement(countRooms).click();
+        driver.findElement(optionsToRooms).click();
+        return this;
+    }
+
+    public RealtHomePage clikToFind() {
+        WebElement btnFind = driver.findElement(findBtn);
+        waitElementVisible(btnFind).click();
+        return this;
+    }
+}
+//*[@id="rooms"]/option[3]
+//*[@id="residentialInputs"]/div[1]/form/div[2]/div[2]/a[2]
